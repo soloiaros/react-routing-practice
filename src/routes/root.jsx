bae74a0180216ac2d093fe5e4ec717969bff5f9a
soloiaros <soloiaros@gmail.com>
2026-05-23 21:17:19 +0300

    style: add visual feedback to sidebar links

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link, useLoaderData, Form, redirect } from "react-router-dom";
+import { Outlet, Link, useLoaderData, Form, redirect, NavLink } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function loader() {
@@ -46,7 +46,13 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink to={`contacts/${contact.id}`} className={({ isActive, isPending }) => (
+                    isActive 
+                      ? "active"
+                      : isPending
+                      ? "pending"
+                      : ""
+                  )}>
                     {contact.firts || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -56,7 +62,7 @@ export default function Root() {
                     )}
                     {' '}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
