34c48877e08c231b023b573e30c28c05131d3cf8
soloiaros <soloiaros@gmail.com>
2026-05-25 15:25:04 +0300

    fix: avoid expanding history stack

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -31,7 +31,8 @@ export default function Root() {
               name="q"
               defaultValue={q}
               onChange={(e) => {
-                submit(e.currentTarget.form);
+                const isFirstSearch = q == null;
+                submit(e.currentTarget.form, {replace: !isFirstSearch});
               }}
             />
             <div id="search-spinner" hidden={!searching} aria-hidden hidden={true} />
